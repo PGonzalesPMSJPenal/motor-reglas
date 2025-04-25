4bd63e06020dc7ed02d0cbd2fa279b3e282fdbb1
Slappy94 <pgonzales.pmsjpenal@gmail.com>
2025-04-25 17:18:32 -0500

    Carga inicial del Proyecto

---
+++ src/main/java/pe/gob/pj/eje/penal/ms_motor_reglas/MsMotorReglasApplication.java
@@ -0,0 +1,13 @@
+package pe.gob.pj.eje.penal.ms_motor_reglas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMotorReglasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMotorReglasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/gob/pj/eje/penal/ms_motor_reglas/MsMotorReglasApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.gob.pj.eje.penal.ms_motor_reglas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMotorReglasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
